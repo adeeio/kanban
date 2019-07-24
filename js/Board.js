d586e1318a2dd2da3d1fd5f4d9483dfe66e41a99
Adrian Ostapkiewicz <ostapkiewicz.adrian@gmail.com>
2019-07-24 20:45:32 +0100

    latest update

--- js/Board.js
+++ js/Board.js
@@ -9,37 +9,33 @@ var board = {
 
 function initSortable(id) {
     var el = document.getElementById(id);
-    var el2 = document.querySelector(".column-container");
-    Sortable.create(el2, {
-        group: 'share-column',
-        sort: true,
-        animation: 100
-    });
+
     Sortable.create(el, {
         group: 'share-card',
         sort: true,
         animation: 150
     });
-    
+
+
 }
 
 document.querySelector('#board .create-column').addEventListener('click', function () {
     var name = inputCheck(prompt('Enter a column name'));
-    var data = new FormData();
 
+    var data = new FormData();
     data.append('name', name);
-    
+
     fetch(prefix + baseUrl + '/column', {
         method: 'POST',
         headers: myHeaders,
         body: data,
     })
-    .then(function(resp) {
-        return resp.json();
-    })
-    .then(function(resp) {
-        var column = new Column(resp.id, name);
-        board.addColumn(column);
-    });
-   
+        .then(function (resp) {
+            return resp.json();
+        })
+        .then(function (resp) {
+            var column = new Column(resp.id, name);
+            board.addColumn(column);
+        });
+
 });
